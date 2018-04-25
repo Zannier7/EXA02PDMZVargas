a61d5bb63dfa9bae18b5715e0cb4bd76746db9af
Marcelo123 <marcelocapcha@upeu.edu.pe>
2018-04-25 14:03:32 -0500

    Initial commit

---
+++ app/src/main/java/com/zannier/app/exa02_pdm_zvargas/MainActivity.java
@@ -0,0 +1,13 @@
+package com.zannier.app.exa02_pdm_zvargas;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
